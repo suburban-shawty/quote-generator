4a381e2757028d978d1013927b2461c8463d0399
Tychelle Jenkins <princess@Tychelles-Air.home>
2019-02-23 17:09:02 -0500

    Project Completed

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 var quotes = [
-    "So, I guess we are who we are for alot of reasons. And maybe we'll never know most of them. But even if we don't have the power to choose where we come from, we can still choose where we go from there. We can still do things. And we can try to feel okay about them. -The Perks of Being a Wallflower",
+    "So, I guess we are who we are for alot of reasons. And maybe we'll never know most of them. But even if we don't have the power to choose where we come from, we can still choose where we go from there. We can still do things. And we can try to feel okay about them.-The Perks of Being a Wallflower",
     "There comes a point where it all becomes too much. When we get too tired to fight anymore. So we give up. That’s when real work begins: to find hope where there seems to be absolutely none at all. -Grey's Anatomy",
     "You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character. -Unkown",
     "But you know, happiness can be found even in the darkest of times, if one only remembers to turn on the light. – Harry Potter and the Prisoner of Azkaban",
